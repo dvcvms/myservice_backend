18b8132d2f761349bc24e8acd54d9a0f2554b21c
dvcvms <tetraedrtrapecia@gmail.com>
2023-04-04 21:01:35 +0300

    feat: add user service and repository

---
+++ src/main/java/ru/backend/myservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package ru.backend.myservice.service.impl;
+
+
+import ru.backend.myservice.dto.RoleDto;
+import ru.backend.myservice.dto.UserDto;
+import ru.backend.myservice.entity.UserEntity;
+import ru.backend.myservice.exception.UserNotFoundException;
+import ru.backend.myservice.mapper.RoleMapper;
+import ru.backend.myservice.mapper.UserMapper;
+import ru.backend.myservice.repository.UserRepository;
+import ru.backend.myservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final RoleMapper roleMapper;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        UserEntity savedUser = userRepository.save(userMapper.toEntity(userDto));
+        return userMapper.toDto(savedUser);
+    }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        UserEntity userIntoDatabase = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        return userMapper.toDto(userIntoDatabase);
+    }
+
+    @Override
+    public UserDto getUserByUsername(String username) {
+        UserEntity userIntoDatabase = userRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException("username: " + username));
+        return userMapper.toDto(userIntoDatabase);
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        UserEntity userIntoDatabase = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("email: " + email));
+        return userMapper.toDto(userIntoDatabase);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto) {
+        return null; // TODO    
+    }
+
+    @Override
+    public String deleteUser(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+
+        return String.format("User with id=%d has been deleted success.", id);
+    }
+
+    @Override
+    public UserDto addRole(UserDto user, RoleDto role) {
+        UserEntity userIntoDatabase = userRepository.findByEmail(user.getEmail())
+                .orElseThrow(() -> new UserNotFoundException("email: " + user.getEmail()));
+
+        userIntoDatabase.addRole(roleMapper.toEntity(role));
+        return userMapper.toDto(userRepository.save(userIntoDatabase));
+    }
+}
