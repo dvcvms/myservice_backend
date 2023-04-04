02df8b49fbdfd25ea21ae8a4e81b6845b0958d13
Максим Волков <101324449+dvcvms@users.noreply.github.com>
2023-04-04 21:36:17 +0300

    feat: add jwt authentication

---
+++ src/main/java/ru/backend/myservice/model/RegisterRequest.java
@@ -0,0 +1,18 @@
+package ru.backend.myservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+}
