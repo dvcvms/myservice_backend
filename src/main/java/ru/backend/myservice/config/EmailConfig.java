367a19a8085905f80dd8ae217bdc9e0571e80b38
dvcvms <tetraedrtrapecia@gmail.com>
2023-04-09 01:00:44 +0300

    feat: add email properties; email config

--- src/main/java/ru/backend/myservice/config/EmailConfig.java
+++ src/main/java/ru/backend/myservice/config/EmailConfig.java
@@ -1,24 +1,28 @@
 package ru.backend.myservice.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import ru.backend.myservice.property.MailProperties;
 
 import java.util.Properties;
 
 @Configuration
+@RequiredArgsConstructor
 public class EmailConfig {
+
+    private final MailProperties mailProperties; // TODO: check on not null property values
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("radagastbrown88@gmail.com"); // TODO: do in properties
-        mailSender.setPassword("nkttqoskdfudlsab");
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getLogin());
+        mailSender.setPassword(mailProperties.getPassword());
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
